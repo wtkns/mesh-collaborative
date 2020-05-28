98f2ea097766cbfd9cecc0200834560ba2ba6e29
james watkins <jms@wtkns.com>
2020-05-27 20:16:31 -0700

    initial abuilder container sketch

---
+++ abuilder/README.md
@@ -0,0 +1,8 @@
+Container definition for Package Building 
+
+Environment: Alpine 3.11
+
+Deps:
+ docker
+
+
---
+++ abuilder/build.sh
@@ -0,0 +1,9 @@
+# docker build abuilder image 
+# start abuilder container
+# download aports
+# generate keys
+# copy keys to local filesystem
+# build packages
+# move packages into package repo (storage bucket?)
+# delete
+
